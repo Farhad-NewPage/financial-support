8c46e5445107110af2ff3a2f8e61f76927380998
Farhad-NewPage <jawad.farhad@pfizer.com>
2024-03-11 11:05:58 +0800

    Putting the check on first column image.

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,27 +2,31 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   // setup image columns
   [...block.children].forEach((row) => {
-    [...row.children].forEach((col) => {
+    [...row.children].forEach((col, colIndex) => {
       $(col).find("p").first().addClass("center");
-      const DesktopLogo = $(col).find("picture").first();
-      $(DesktopLogo)
-        .children()
-        .each(function (index, element) {
-          if (element.tagName === "SOURCE") {
-            $(element).attr("media", "(min-width: 600px)");
-          } else if (element.tagName === "IMG") {
-            $(element).attr({ width: 201, height: 150 });
-          }
-        });
+      if (colIndex === 0) {
+        const DesktopLogo = $(col).find("picture").first();
+        $(DesktopLogo)
+          .children()
+          .each(function (index, element) {
+            if (element.tagName === "SOURCE") {
+              $(element).attr("media", "(min-width: 600px)");
+            } else if (element.tagName === "IMG") {
+              $(element).attr({ width: 201, height: 150 });
+            }
+          });
 
-      const mobileLogo = $(col).find("picture:eq(1)");
-      if (mobileLogo.length) {
-        $(mobileLogo).addClass("icon icon-pot-logo");
-      } else {
-        const mobileLogo = $(row).children(":first").find("p > picture:eq(1)");
-        $(mobileLogo).addClass("icon icon-pot-logo");
+        const mobileLogo = $(col).find("picture:eq(1)");
+        if (mobileLogo.length) {
+          $(mobileLogo).addClass("icon icon-pot-logo");
+        } else {
+          const mobileLogo = $(row)
+            .children(":first")
+            .find("p > picture:eq(1)");
+          $(mobileLogo).addClass("icon icon-pot-logo");
 
-        $(col).prepend(mobileLogo);
+          $(col).prepend(mobileLogo);
+        }
       }
     });
   });

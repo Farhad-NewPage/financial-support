c386a294296574c39d606a4fab718bc8a4a8e736
Farhad-NewPage <jawad.farhad@pfizer.com>
2024-03-19 16:25:55 +0500

    Form Submission url updated.

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -38,7 +38,8 @@ async function submitForm(form) {
   const payload = constructPayload(form);
   payload.timestamp = new Date().toJSON();
   const resp = await fetch(
-    `https://form.aem.page/main--helix-website--adobe${form.dataset.action}`,
+    // `https://form.aem.page/main--helix-website--adobe${form.dataset.action}`,
+    `https://admin.hlx.page/form/farhad-newpage/financial-support/en${form.dataset.action}.json`,
     {
       method: "POST",
       cache: "no-cache",
